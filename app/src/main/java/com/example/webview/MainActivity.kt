74a4b98640d6298b00532aea0e9390fcdafa3059
ulula <sukmadewiwulansari@gmail.com>
2023-01-21 22:36:48 +0700

    Initial commit

---
+++ app/src/main/java/com/example/webview/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.webview
+
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_unissula -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, MenuFragment())
+                    .addToBackStack(null)
+                    .commit()
+                return true
+
+            }
+            R.id.menu_republika -> {
+                val i = Intent(this, MenuActivity::class.java)
+                startActivity(i)
+                return true
+            }
+            else -> return true
+        }
+    }
+
+}
